019e1ef8ce939a08290437ab83b922c1eacf49be
samiudeen96 <samiudeen96amm@gmail.com>
2026-09-24 16:14:14 +0400

    update

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -32,7 +32,7 @@ export default function App() {
   );
 }
 
-// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response.
+// Shopify needs Remix to catch some thrown responses, so that their headers are included in the response
 export function ErrorBoundary() {
   return boundary.error(useRouteError());
 }
